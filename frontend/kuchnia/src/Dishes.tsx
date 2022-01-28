f13fae3380256d947449a313848de3995c59966d
Jeremi Sobierski <tbuli1112@gmail.com>
2022-01-28 21:18:55 +0100

    Adds the DishList component

---
+++ frontend/kuchnia/src/Dishes.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Row, useRemoteArray } from "./remote"
+
+interface Dish extends Row {
+    name: string,
+    lastMade: Date,
+}
+
+export default function DishList() {
+    const dishes = useRemoteArray<Dish>('/dishes');
+    if (dishes.data === undefined) {
+        return <div>Loading</div>;
+    }
+    return (
+        <div>
+            {dishes.data.map(dish => <div key={dish.id}>
+                {dish.lastMade}: {dish.name}
+                <button onClick={() => dishes.update("done", dish.id)}>Done</button>
+            </div>)}
+        </div>
+    );
+}
