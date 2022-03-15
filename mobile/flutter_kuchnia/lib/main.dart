5503bcce2deee73c3636f3bddaa7d555d13e1d50
Jeremi Sobierski <tbuli1112@gmail.com>
2022-03-15 14:37:18 +0100

    adds theme

--- mobile/flutter_kuchnia/lib/main.dart
+++ mobile/flutter_kuchnia/lib/main.dart
@@ -5,22 +5,56 @@ void main() {
   runApp(const KitchenApp());
 }
 
-class KitchenApp extends StatelessWidget {
+class KitchenApp extends StatefulWidget {
   const KitchenApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<KitchenApp> createState() => _KitchenAppState();
+}
+
+class _KitchenAppState extends State<KitchenApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Kuchnia'),
-          backgroundColor: const Color.fromRGBO(255, 0, 0, 1),
-          elevation: 1,
+      home: DefaultTabController(
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(tabs: [Text('Dishes'), Text('List')]),
+            elevation: 1,
+          ),
+          body: const TabBarView(children: [
+            Align(child: Dishes(), alignment: Alignment.topCenter),
+            Text('data')
+          ]),
         ),
-        body: const Dishes(),
+        length: 2,
       ),
+      theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 24, 78, 119),
+          cardColor: const Color.fromARGB(255, 149, 246, 233),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 24, 78, 119),
+          appBarTheme: const AppBarTheme(
+              color: Color.fromARGB(255, 153, 217, 140),
+              foregroundColor: Colors.black),
+          popupMenuTheme: const PopupMenuThemeData(color: Colors.white),
+          tabBarTheme: const TabBarTheme(labelColor: Colors.black),
+          textTheme: const TextTheme(
+              headline6: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19))),
     );
   }
-}
 
+  @override
+  void initState() {
+    super.initState();
+    print('app init');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('app unmount');
+  }
+}
